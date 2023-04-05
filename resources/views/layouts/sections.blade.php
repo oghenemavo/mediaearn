147e19cb1be898433f39b5af974127a8842e096a
Mavi Onogomuho <mavi.onogomuho@titantrustbank.com>
2023-04-05 12:02:12 +0100

    setup tawk to

--- resources/views/layouts/sections.blade.php
+++ resources/views/layouts/sections.blade.php
@@ -73,6 +73,20 @@
             })
         </script>
 
+        <!--Start of Tawk.to Script-->
+        <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/642d53d04247f20fefe9f26c/1gt8guq5i';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+        })();
+        </script>
+        <!--End of Tawk.to Script-->
+
         @stack('scripts')
     </body>
 </html>
